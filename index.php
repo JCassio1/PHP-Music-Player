c7b33e3dbc62abb894259e2e998eb11215b6b845
Mr. Robot <joselsondias@hotmail.com>
2019-07-03 15:53:26 +0100

    Add left side bar and elements. (create songs database)

--- index.php
+++ index.php
@@ -26,6 +26,39 @@ else{
 
         <div id="topContainer">
 
+           <div id="navBarContainer">
+             <nav class="navBar">
+               <a href="index.php" class="logo">
+                 <img src="assets/images/logo/jdMusicLogo.png" alt="jdMusicLogo">
+               </a>
+
+               <div class="group">
+                 <div class="navItem">
+                   <a href="search.php" class="navItemLink">Search <img src="assets/images/icons/search.png" class="icon" alt="search"></a>
+                 </div>
+
+               </div>
+
+               <div class="group">
+
+                 <div class="navItem">
+                   <div class="navItem">
+                     <a href="browse.php" class="navItemLink">Browse</a>
+                   </div>
+
+                   <div class="navItem">
+                     <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                   </div>
+                   <div class="navItem">
+                     <a href="Profile.php" class="navItemLink">Joselson Dias</a>
+                   </div>
+
+                 </div>
+               </div>
+             </nav>
+
+           </div>
+
         </div>
 
         <div id="playingBarContainer">
@@ -115,11 +148,8 @@ else{
                       </div>
                     </div>
                   </div>
-
                 </div>
-
               </div>
-
             </div>
         </div>
 
