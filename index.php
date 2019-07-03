574a0a6ca1f6a24c5f9597927ab9a0e4f4e2e260
Mr. Robot <joselsondias@hotmail.com>
2019-07-03 12:28:47 +0100

    Add containers layout, fix volume buttons and minimal layout adjustments

--- index.php
+++ index.php
@@ -22,73 +22,107 @@ else{
       <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-      <div id="playingBarContainer">
+      <div id="mainContainer">
 
-          <div id="playingBar">
+        <div id="topContainer">
 
-            <div id="nowPlayingLeftSide">
-              <div class="content">
-                <span class="albumLink">
-                  <img src="https://i1.sndcdn.com/artworks-000235926242-q4hojq-t500x500.jpg" class="albumArtwork" alt="nowPlaying">
-                </span>
-              </div>
-            </div>
+        </div>
 
-            <div id="nowPlayingCenterSide">
+        <div id="playingBarContainer">
 
-                <div class="content playerControls">
+            <div id="playingBar">
 
-                  <div class="buttons">
+              <div id="nowPlayingLeftSide">
+                <div class="content">
+                  <span class="albumLink">
+                    <img src="https://i1.sndcdn.com/artworks-000235926242-q4hojq-t500x500.jpg" class="albumArtwork" alt="nowPlaying">
+                  </span>
 
-                    <button class="controlButton shuffle" title="shuffle button">
-                      <img src="assets/images/icons/shuffle.png" alt="shuffle">
-                    </button>
+                  <div class="trackInformation">
 
-                    <button class="controlButton previous" title="previous button">
-                      <img src="assets/images/icons/previous.png" alt="previous">
-                    </button>
+                    <span class="trackName">
+                      <span> Mandume</span>
+                    </span>
 
-                    <button class="controlButton play" title="play button">
-                      <img src="assets/images/icons/play.png" alt="play">
-                    </button>
+                    <span class="artistName">
+                      <span> Milly Mizzy</span>
+                    </span>
 
-                    <button class="controlButton pause" title="pause button" style="display:none">
-                      <img src="assets/images/icons/pause.png" alt="pause">
-                    </button>
-
-                    <button class="controlButton next" title="next button">
-                      <img src="assets/images/icons/next.png" alt="next">
-                    </button>
-
-                    <button class="controlButton repeat" title="repeat button">
-                      <img src="assets/images/icons/repeat.png" alt="repeat">
-                    </button>
-                  </div>
-
-                  <div class="playbackBar">
-                    <span class="progressTime current">0.00</span>
-
-                    <div class="progressBar">
-
-                      <div class="progressBarBackground">
-                        <div class="progress">
-
-                        </div>
-                      </div>
-
-                    </div>
-
-                    <span class="progressTime remaining">0.00</span>
                   </div>
 
                 </div>
+              </div>
+
+              <div id="nowPlayingCenterSide">
+
+                  <div class="content playerControls">
+
+                    <div class="buttons">
+
+                      <button class="controlButton shuffle" title="shuffle button">
+                        <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                      </button>
+
+                      <button class="controlButton previous" title="previous button">
+                        <img src="assets/images/icons/previous.png" alt="previous">
+                      </button>
+
+                      <button class="controlButton play" title="play button">
+                        <img src="assets/images/icons/play.png" alt="play">
+                      </button>
+
+                      <button class="controlButton pause" title="pause button" style="display:none">
+                        <img src="assets/images/icons/pause.png" alt="pause">
+                      </button>
+
+                      <button class="controlButton next" title="next button">
+                        <img src="assets/images/icons/next.png" alt="next">
+                      </button>
+
+                      <button class="controlButton repeat" title="repeat button">
+                        <img src="assets/images/icons/repeat.png" alt="repeat">
+                      </button>
+                    </div>
+
+                    <div class="playbackBar">
+                      <span class="progressTime current">0.00</span>
+
+                      <div class="progressBar">
+
+                        <div class="progressBarBackground">
+                          <div class="progress">
+                          </div>
+                        </div>
+                      </div>
+
+                      <span class="progressTime remaining">0.00</span>
+                    </div>
+
+                  </div>
+              </div>
+
+              <div id="nowPlayingRightSide">
+                <div class="volumeBar">
+                  <button class="controlButton volume" name="volume slider">
+                    <img src="assets/images/icons/volume.png" alt="volume slider">
+                  </button>
+
+                  <div class="progressBar">
+
+                    <div class="progressBarBackground">
+                      <div class="progress">
+
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+
             </div>
+        </div>
 
-            <div id="nowPlayingRightSide">
-
-            </div>
-
-          </div>
       </div>
     </body>
   </html>
